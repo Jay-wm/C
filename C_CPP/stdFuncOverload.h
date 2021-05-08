36e3474025bc35abde10448712f26ba9c02db475
Jay <Jay_x6015@outlook.com>
2021-05-08 11:09:19 +0800

    overload function: strcpy

---
+++ C_CPP/stdFuncOverload.h
@@ -0,0 +1,14 @@
+/*
+ * 该文件用来对 ISO 标准 C/C++ 中有缺陷的函数进行重载
+ * 1、char *strcpy(char *dest, const char *src);
+ *
+*/
+
+#ifndef STDFUNCOVERLOAD_H
+#define STDFUNCOVERLOAD_H
+#include <string.h>
+
+char *strcpy(char *dest, const char *src);
+
+
+#endif /* STDFUNCOVERLOAD_H */
